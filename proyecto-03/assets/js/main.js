dc17f02cff49a71901608ead86689bbf9a7aedbb
walterdiaz78 <walterdiaz78@gmail.com>
2024-07-25 19:20:27 -0300

    proyecto 3 ok

---
+++ proyecto-03/assets/js/main.js
@@ -0,0 +1,8 @@
+let velocidad = 135;
+const limite = 60;
+
+if(velocidad > limite){
+    console.log("Tienes que reducir la velocidad, vas a "+velocidad+"Km/h");
+    }else{
+        console.log("Estas dentro de la velocidad permitida");
+}
