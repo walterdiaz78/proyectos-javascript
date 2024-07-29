e8d8099171d33c651f32118d65d7cbb3ec3be639
walterdiaz78 <walterdiaz78@gmail.com>
2024-07-29 16:12:57 -0300

    Proyecto 7 Js ok

--- proyecto-07/assets/js/main.js
+++ proyecto-07/assets/js/main.js
@@ -1,8 +1,38 @@
 
 const actualYear = new Date().getFullYear();
-alert(actualYear)
+
+let year = 1990;
+
+let result = 0;
+
+do {
+    year = parseInt(prompt("¿En que año naciste?", year));
+    
+}while (year < (actualYear - 150) || year > actualYear);
 
 
 
+result = actualYear - year;
+
+alert("Tienes " +result, "Años");
 
 
+
+// Practica.
+
+
+// const añoActual = new Date().getFullYear();
+
+// let nacimiento = 1990;
+
+// let edad = 0;
+
+// do {
+//     nacimiento = parseInt(prompt("En que año naciste?", nacimiento));
+    
+// } while (nacimiento < 1910 || nacimiento > añoActual);
+
+
+// edad = añoActual - nacimiento;
+
+// alert("Tienes "+ edad + "años de edad")
