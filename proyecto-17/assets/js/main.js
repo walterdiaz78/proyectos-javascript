dda07f83cf63e1739cd1613d44612a714a29b19c
walterdiaz78 <walterdiaz78@gmail.com>
2024-08-07 11:47:40 -0300

    project 17 ok

---
+++ proyecto-17/assets/js/main.js
@@ -0,0 +1,23 @@
+
+window.addEventListener("DOMContentLoaded", event => {
+
+    let card = document.querySelectorAll(".layout__card");
+    
+
+    card.forEach(c => {
+        let header = c.querySelector(".card__header");
+        let description = c.querySelector(".content__description");
+        let btn = c.querySelector(".content__btn");
+    
+
+    btn.addEventListener("click", () => {
+
+        c.style.boxShadow = "0px 0px 14px black";
+        header.style.backgroundColor = "#1B62CA";
+        description.style.opacity = "0.8";
+        btn.style.backgroundColor = "white";
+        btn.style.color = "#1b62CA";
+    });
+});
+
+});
