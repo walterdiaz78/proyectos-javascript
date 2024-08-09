9d222aa1fd9df42cbd7f8eaa2c3e1b8292890f21
walterdiaz78 <walterdiaz78@gmail.com>
2024-08-09 18:23:19 -0300

    project 22 js ok

---
+++ proyecto-22/assets/js/main.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    let log = console.log;
+
+    let input = document.querySelector(".main__input");
+    let text = document.querySelector(".main__text");
+
+    
+
+
+    input.addEventListener("keydown", (e) =>{
+
+        let search = input.value;
+
+        if(search.trim() != "" && e.code == "Enter"){
+            let regExp = new RegExp(search, "gi");
+
+            text.innerHTML = text.textContent.replace(regExp, "<span class='text__search'>$&</span>");
+        };
+    });
+
+
+
+});
