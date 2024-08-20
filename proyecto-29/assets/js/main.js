618a9f927e7587f88b86c1ed284783e7ed60948e
walterdiaz78 <walterdiaz78@gmail.com>
2024-08-20 16:12:08 -0300

    project 29 js finished

--- proyecto-29/assets/js/main.js
+++ proyecto-29/assets/js/main.js
@@ -1,6 +1,26 @@
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
 
+    let controls = document.querySelectorAll(".form__control");
 
+    controls.forEach(formcontrol => {
+
+        let label = formcontrol.firstElementChild;
+        let input = formcontrol.lastElementChild;
+
+        input.addEventListener("focus", () => {
+            label.classList.remove("blur");
+            label.classList.add("focus");
+        });
+        
+
+        input.addEventListener("blur", () => {
+            if(input.value.trim() == ""){
+                label.classList.remove("focus");
+                label.classList.add("blur");
+            };
+
+        });
+    });
 
 });
