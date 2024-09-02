3eb0f4094341a01a8cefe8b60c70419c84ac3eab
walterdiaz78 <walterdiaz78@gmail.com>
2024-09-02 15:36:17 -0300

    Project 37 js finished

---
+++ proyecto-37/assets/js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", () => {
+
+    let btn = document.querySelector(".nav__btn");
+    let plus = document.querySelector(".btn__ico-plus");
+    let close = document.querySelector(".btn__ico-close");
+    let list = document.querySelector(".nav__list");
+
+    btn.addEventListener("click", () => {
+        plus.classList.toggle('btn__show');
+        close.classList.toggle('btn__show');
+        list.classList.toggle('list__open');
+
+    })
+
+    // let btn = document.querySelector(".nav__btn");
+    // let nav = document.querySelector(".nav__list");
+    // btn.addEventListener("mouseover", () => {
+    //     nav.classListadd(".list__open")
+    // });
+});
